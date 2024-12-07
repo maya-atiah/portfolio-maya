a777c53ae1ceb6ddf799af49ecac24d506e6c1c3
Maya <maya.atiah.99@outlook.com>
2024-12-07 12:14:18 +0200

    test

--- components/layout/About.js
+++ components/layout/About.js
@@ -8,7 +8,7 @@ const About = () => {
     <div className='about-container' id='about' >
           <div className='square2'></div>
           <h1>about <span>Myself</span></h1>
-          <h3>A Full-Stack Developer experienced with popular frameworks and libraries such as React, Laravel, and NodeJs. Attended Codi-Tech, a web development boot-camp where I learned new technical and soft skills including new languages, group work, managing a team, and following the AGILE Methodology in every project. In addition, I am  interested in designing and structuring wireframes.</h3>
+          <h3>I am a Full Stack </h3>
     </div>
   )
 }
